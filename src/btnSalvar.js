aa2d741c088dcc98887fc1ddc5d4147d4f25afac
gustavo.castro01@fatec.sp.gov.br <gustavo.castro01@fatec.sp.gov.br>
2024-09-01 13:05:17 -0300

    InputOff

--- src/btnSalvar.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import { StyleSheet, Button, SafeAreaView } from 'react-native';
-
-// const btnSalvar= () => {
-//     <Button
-//     title='Resultado'
-//     onPress={}/>
-// }
